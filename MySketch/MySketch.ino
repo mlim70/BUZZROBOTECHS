225af700e0af77c9c46ab06363cd96972b7689a9
Matthew <78234838+mlim70@users.noreply.github.com>
2025-03-28 20:51:53 -0400

    Update MySketch.ino

--- MySketch/MySketch.ino
+++ MySketch/MySketch.ino
@@ -1,1 +1,10 @@
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
 
+void loop() {
+  // print out a message every second:
+  Serial.println("Hello, Arduino!");
+  delay(1000);
+}
